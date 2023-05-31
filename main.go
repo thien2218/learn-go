294dfcb042ddbd2d77d856a8b6d5ccbe087fcbf8
Thien Huynh <thien1367526@gmail.com>
2023-05-31 17:04:56 +0700

    Read file functions and init MinCuts algorithm

--- main.go
+++ main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/thien2218/learn-go/algorithms"
+	"bufio"
+	// "fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	// "github.com/thien2218/learn-go/algorithms"
 )
 
 func main() {
-	arr := []int{9, 11, 14, 8, 19, 4, 2, 17, 16, 7, 12, 6, 5, 15, 13, 3, 1, 20, 18, 10}
 
-	num := algorithms.QuickSelect(arr, 0, len(arr)-1, 19)
-	fmt.Println(num)
 }
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readGraphFromFile(graph map[int][]int, filename string) {
+	file, err := os.Open(filename)
+	handleError(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+		words := strings.Fields(line)
+
+		for j, word := range words {
+			n, err := strconv.Atoi(word)
+			handleError(err)
+
+			if j == 0 {
+				graph[i+1] = make([]int, 0, 200)
+			} else {
+				graph[i+1] = append(graph[i+1], n)
+			}
+		}
+	}
+
+	handleError(scanner.Err())
+}
+
+// func readNumsFromFile(arr []int, filename string) {
+// 	file, err := os.Open(filename)
+// 	handleError(err)
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+// 	for i := 0; scanner.Scan(); i++ {
+// 		txt := scanner.Text()
+// 		num, err := strconv.Atoi(txt)
+// 		handleError(err)
+// 		arr = append(arr, num)
+// 	}
+
+// 	handleError(scanner.Err())
+// }
